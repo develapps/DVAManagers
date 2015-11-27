ef871659c463fbd4cdf909b512c77b2c5881540f
Pablo Romeu <pablo.romeu@develapps.com>
2015-11-27 09:07:42 +0100

    Initial commit

---
+++ Example/DVAManagers/DVAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DVAAppDelegate.h
+//  DVAManagers
+//
+//  Created by Pablo Romeu on 11/27/2015.
+//  Copyright (c) 2015 Pablo Romeu. All rights reserved.
+//
+
+@import UIKit;
+
+@interface DVAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/DVAManagers/DVAViewController.h
@@ -0,0 +1,13 @@
+//
+//  DVAViewController.h
+//  DVAManagers
+//
+//  Created by Pablo Romeu on 11/27/2015.
+//  Copyright (c) 2015 Pablo Romeu. All rights reserved.
+//
+
+@import UIKit;
+
+@interface DVAViewController : UIViewController
+
+@end
