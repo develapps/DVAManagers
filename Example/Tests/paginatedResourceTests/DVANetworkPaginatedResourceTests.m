7f4b530416093277789a5d5eb68d8c4759f7eda1
Pablo Romeu <pablo.romeu@develapps.com>
2015-12-04 17:10:25 +0100

    - Cache defaults to enabled.

--- Example/Tests/paginatedResourceTests/DVANetworkPaginatedResourceTests.m
+++ Example/Tests/paginatedResourceTests/DVANetworkPaginatedResourceTests.m
@@ -197,6 +197,106 @@ static NSString *const theSecondPage=@"/v1/fake/?page=2";
     [self waitForExpectationsWithTimeout:10 handler:nil];
 }
 
+-(void)testPaginationWithCacheThenNetwork{
+    if (kStubsEnabled) {
+        NSDictionary *stubOptions = @{
+                                      @(kDVANetworkStubConfiguratorValueDebug)       : @(kStubsEnabled),
+                                      @(kDVANetworkStubConfiguratorValueEndpoint)    : theEndPoint,
+                                      @(kDVANetworkStubConfiguratorValueType)        : @(kDVAStubGET),
+                                      @(kDVANetworkStubConfiguratorValueJson)        : [DVANetworkStubConfigurator dva_readJsonFile:@"FirstPage" inBundle:[NSBundle bundleForClass:[self class]]],
+                                      
+                                      };
+        
+        DVANetworkStubConfigurator *configurator = [DVANetworkStubConfigurator configuratorWithDictionary:stubOptions];
+        NSDictionary *secondStubOptions = @{
+                                            @(kDVANetworkStubConfiguratorValueDebug)       : @(kStubsEnabled),
+                                            @(kDVANetworkStubConfiguratorValueEndpoint)    : theSecondPage,
+                                            @(kDVANetworkStubConfiguratorValueType)        : @(kDVAStubGET),
+                                            @(kDVANetworkStubConfiguratorValueJson)        : [DVANetworkStubConfigurator dva_readJsonFile:@"SecondPage" inBundle:[NSBundle bundleForClass:[self class]]],
+                                            
+                                            };
+        
+        DVANetworkStubConfigurator *secondConfigurator = [DVANetworkStubConfigurator configuratorWithDictionary:secondStubOptions];
+        [self dva_stubOperationWithOptionsArray:@[configurator,secondConfigurator]];
+        
+    }
+    _paginatedResource = [[DVANetworkPaginatedResource alloc]  initWithApiProvider:self.manager
+                                                                   itemsArrayBlock:^NSArray *(id jsonObject) {
+                                                                       return jsonObject[@"results"];
+                                                                   } itemsCountBlock:^NSInteger(id jsonObject) {
+                                                                       return [jsonObject[@"count"] integerValue];
+                                                                       
+                                                                   } nextPageBlock:^NSString *(id jsonObject) {
+                                                                       return jsonObject[@"next"];
+                                                                       
+                                                                   }];
+    [_paginatedResource setNextURL:[theUrl stringByAppendingString:theEndPoint]];
+    [_paginatedResource setDebug:YES];
+    [[DVACache sharedInstance] removeAllDiskCachedData];
+    [[DVACache sharedInstance] removeAllMemoryCachedData];
+    
+    XCTestExpectation *expectation = [self expectationWithDescription:@"firstPage"];
+    XCTestExpectation *secondExpectation = [self expectationWithDescription:@"secondPage"];
+    XCTestExpectation* thirdExpectation = [self expectationWithDescription:@"firstPageCached"];
+    XCTestExpectation* fourthExpectation = [self expectationWithDescription:@"firstPageCachedSecondTime"];
+    
+    [_paginatedResource nextBatch:^(BOOL fromCache, NSArray *items, NSInteger totalItemsCount, NSError *error) {
+        XCTAssert([items count]==3,@"ERROR: Items count for first page should be 3 not %lu",(unsigned long)[items count]);
+        XCTAssert(totalItemsCount==6,@"ERROR: Total count for first page should be 6 not %lu",(unsigned long)totalItemsCount);
+        XCTAssert(error==nil,@"ERROR: This should not fail: %@",error);
+        XCTAssert(!fromCache,@"ERROR: This should not be cached: %i",fromCache);
+        NSString *secondUrl =[theUrl stringByAppendingString:theSecondPage];
+        XCTAssert([_paginatedResource.nextURL isEqualToString:secondUrl],@"ERROR: Next page should be %@ not %@",secondUrl,_paginatedResource.nextURL);
+        [expectation fulfill];
+        [_paginatedResource nextBatch:^(BOOL fromCache, NSArray *items, NSInteger totalItemsCount, NSError *error) {
+            XCTAssert([items count]==3,@"ERROR: Items count for second page should be 3 not %lu",(unsigned long)[items count]);
+            XCTAssert(totalItemsCount==6,@"ERROR: Total count for second page should be 6 not %lu",(unsigned long)totalItemsCount);
+            XCTAssert(error==nil,@"ERROR: This should not fail: %@",error);
+            XCTAssert([_paginatedResource.nextURL isEqual:[NSNull null]],@"ERROR: Next page should be NSNull not %@",_paginatedResource.nextURL);
+            XCTAssert(!fromCache,@"ERROR: This should not be cached: %i",fromCache);
+            
+            [secondExpectation fulfill];
+            
+            _paginatedResource = [[DVANetworkPaginatedResource alloc]  initWithApiProvider:self.manager
+                                                                           itemsArrayBlock:^NSArray *(id jsonObject) {
+                                                                               return jsonObject[@"results"];
+                                                                           } itemsCountBlock:^NSInteger(id jsonObject) {
+                                                                               return [jsonObject[@"count"] integerValue];
+                                                                               
+                                                                           } nextPageBlock:^NSString *(id jsonObject) {
+                                                                               return jsonObject[@"next"];
+                                                                               
+                                                                           }];
+            [_paginatedResource setNextURL:[theUrl stringByAppendingString:theEndPoint]];
+            [_paginatedResource setDebug:YES];
+            [_paginatedResource setCacheBehaviour:DVAPaginatedResponseCacheThenNetwork];
+            __block BOOL firstCall = YES;
+            [_paginatedResource nextBatch:^(BOOL fromCache, NSArray *items, NSInteger totalItemsCount, NSError *error) {
+                XCTAssert([items count]==3,@"ERROR: Items count for first page should be 3 not %lu",(unsigned long)[items count]);
+                XCTAssert(totalItemsCount==6,@"ERROR: Total count for first page should be 6 not %lu",(unsigned long)totalItemsCount);
+                XCTAssert(error==nil,@"ERROR: This should not fail: %@",error);
+                if (firstCall) {
+                    XCTAssert(fromCache,@"ERROR: This should be cached: %i",fromCache);
+                    [thirdExpectation fulfill];
+                    firstCall = NO;
+                }
+                else
+                {
+                    XCTAssert(!fromCache,@"ERROR: This should not be cached: %i",fromCache);
+                    [fourthExpectation fulfill];
+                }
+            }];
+        }];
+    }];
+    
+    
+    
+    
+    [self waitForExpectationsWithTimeout:10 handler:nil];
+}
+
+
+
 
 @end
 
